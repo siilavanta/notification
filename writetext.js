3286bc7154ffadf153ad9ec81f98b2df7c65306a
siilavanta <53584681+siilavanta@users.noreply.github.com>
2020-09-13 21:31:52 +0600

    Update writetext.js

--- writetext.js
+++ writetext.js
@@ -1,1 +1,7 @@
-alert('Welcome to WriteText App.')
+//alert('Welcome to WriteText App.')
+var eddit_head = document.getElementById('eddit_head')
+var div = document.createElement('span')
+div.setAttribute('onclick', 'addValue(text), autoFocus() ')
+div.setAttribute('class', 'eddit_head_item')
+div.innerText = 'text'
+eddit_head.appendChild(div)
