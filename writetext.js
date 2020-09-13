6c5455fe051688bb58f55e94a6ea7564c94ff5ee
siilavanta <53584681+siilavanta@users.noreply.github.com>
2020-09-13 21:47:58 +0600

    Update writetext.js
    backtik add

--- writetext.js
+++ writetext.js
@@ -1,7 +1,6 @@
-//alert('Welcome to WriteText App.')
-var eddit_head = document.getElementById('eddit_head')
+    var eddit_head = document.getElementById('eddit_head')
 var div = document.createElement('span')
-div.setAttribute('onclick', 'addValue('tex'), autoFocus() ')
+div.setAttribute('onclick', `addValue('text'), autoFocus()`)
 div.setAttribute('class', 'eddit_head_item')
 div.innerText = 'text'
 eddit_head.appendChild(div)
