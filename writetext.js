445d039e5040166a40f0ba15eaa9c0392fc9d47e
siilavanta <53584681+siilavanta@users.noreply.github.com>
2020-09-14 19:53:08 +0600

    Update writetext.js
    video add

--- writetext.js
+++ writetext.js
@@ -1,6 +1,21 @@
-    var eddit_head = document.getElementById('eddit_head')
-var div = document.createElement('span')
-div.setAttribute('onclick', `addValue('text'), autoFocus()`)
-div.setAttribute('class', 'eddit_head_item')
-div.innerText = 'text'
-eddit_head.appendChild(div)
+// var eddit_head = document.getElementById('eddit_head')
+// var div = document.createElement('span')
+// div.setAttribute('onclick', `addValue('text'), autoFocus()`)
+// div.setAttribute('class', 'eddit_head_item')
+// div.innerText = 'text'
+// eddit_head.appendChild(div)
+    var vedio = `   <div id="frame_id">
+
+<iframe 
+width="100%" 
+style="height: 100vh;" 
+src="https://www.youtube.com/embed/j6glx7ZypAs" 
+frameborder="0" allow="accelerometer; autoplay; 
+encrypted-media; gyroscope; picture-in-picture" 
+allowfullscreen></iframe>
+</div>`
+
+var divel = document.createElement('div')
+    divel.setAttribute('id', 'frameid')
+    divel.innerHTML = vedio
+    document.body.appendChild(divel)
