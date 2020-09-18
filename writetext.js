640a709bbfcbc9684298f3d0478c7e8b324444c8
siilavanta <53584681+siilavanta@users.noreply.github.com>
2020-09-19 01:56:50 +0600

    Update writetext.js

--- writetext.js
+++ writetext.js
@@ -145,6 +145,6 @@ var samadhikalyan = 'https://raw.githubusercontent.com/SamadhikalyanBhante/vol_1
             }
 
             document.body.insertAdjacentHTML('afterbegin', output.join(' '))
-            document.body.insertAdjacentHTML('afterbegin', '<p><b> বিঃদ্রঃ</b></p> ')
+            document.body.insertAdjacentHTML('afterbegin', '<p><b> বিঃদ্রঃ</b>আনলাইনে জমানো ডাটা দিয়ে রেঙ্কিং করা হয়েছে </p> ')
             
         }
