4f3b0fcae231bad0e45786345563b7ec35548391
siilavanta <53584681+siilavanta@users.noreply.github.com>
2020-10-10 14:49:19 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var head = `<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ani dim</title>
+    <link rel="stylesheet" href="style.css">
+</head>`
+document.body.append(head)
