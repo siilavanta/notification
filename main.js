78f89081b76b9169373914ba0d9c88e63a4e5912
siilavanta <53584681+siilavanta@users.noreply.github.com>
2020-10-11 16:02:29 +0600

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ var head = `<head>
     <link rel="stylesheet" href="style.css">
 </head>`;
 var txt = `<h2>bh xb sx hsx </h2>`
+var form = `<form method="POST" enctype="multipart/form-data">
+File to upload: <input type="file" name="uploadfile">&nbsp;&nbsp;
+<input type="submit" value="Press to Upload..."> to upload the file!
+</form>`
 //document.head.insertAdjacentHTML('afterbegin', head);
 document.body.insertAdjacentHTML('afterbegin', txt);
+document.body.insertAdjacentHTML('afterbegin', form);
 
